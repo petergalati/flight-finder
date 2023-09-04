ee06eb05b3b3169d850388c8648b1d37a3c23b12
Peter Galati <peter8galati@gmail.com>
2023-09-04 17:52:43 +0100

    trying different amadeus api to debug broken cheapest-dates

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Form
 from typing import Annotated
+# from pydantic import BaseModel
 from datetime import date
 import httpx
+
+
 
 app = FastAPI()
 
@@ -14,6 +17,8 @@ async def get_amadeus_token(
     grant_type: Annotated[str, Form()],
     client_id: Annotated[str, Form()],
     client_secret: Annotated[str, Form()]
+
+    
     ):
 
     url = "https://test.api.amadeus.com/v1/security/oauth2/token"
@@ -29,11 +34,14 @@ async def get_amadeus_token(
 
     if r.status_code == httpx.codes.OK:
         data = r.json()
-        return {"message": "POST successful", "data": data.get("access_token")}
+        return {"message": "POST successful", "token": data.get("access_token")}
     else:
         return {"message": f"POST failed with code {r.status_code}"}
     
-@app.get("/trip-data/amadeus/flights/inspiration")
+
+    
+# @app.get("/trip-data/amadeus/flights/inspiration")
+@app.get("/api/flight-finder/inspiration/cheapest-dates")
 async def get_flight_inspo(
     origin: str,
     destination: str,
@@ -42,12 +50,15 @@ async def get_flight_inspo(
     API_SECRET = "Wo1S8xK2rMfmU8WJ"
     BASE_URL = "https://test.api.amadeus.com/v1"
 
-    api_token = await get_amadeus_token("client_credentials", API_KEY, API_SECRET)
+    api_token = await get_amadeus_token(
+        grant_type="client_credentials",
+        client_id=API_KEY,
+        client_secret=API_SECRET
+        )
+    api_token = api_token.get("token")
 
-    # not sure yet
     endpoint = "/shopping/flight-dates"
 
-    # not sure yet
     headers = {
         "Authorization": f"Bearer {api_token}"
     }
@@ -65,5 +76,8 @@ async def get_flight_inspo(
                 return results
         else:
             return {"message": f"API GET request failed with code {r.status_code}"}
+        
+
+@app.get("/api/flight-finder/inspiration/cheapest-dates")
 
 
